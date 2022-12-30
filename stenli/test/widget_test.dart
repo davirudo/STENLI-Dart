a468d7e494bed1bb6d5688e5a5a51920d2eaee91
David Diamanta <dantadiamanta@gmail.com>
2022-12-30 09:24:36 +0700

    perbaikan kecil

--- stenli/test/widget_test.dart
+++ stenli/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:stenli_dart/main.dart';
+import 'package:stenli/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
