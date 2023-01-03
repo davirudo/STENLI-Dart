59d9131a3b87b5ae4943c731718420cc88b51466
David Diamanta <dantadiamanta@gmail.com>
2023-01-03 23:39:17 +0700

    fixing some pallete color

--- stenli/lib/config/app_color.dart
+++ stenli/lib/config/app_color.dart
@@ -8,11 +8,11 @@ class AppColor {
   static const chart = Color.fromARGB(255, 177, 179, 220);
 
   static const sred = Color.fromARGB(255, 218, 106, 106);
-  static const red = Color.fromARGB(255, 255, 154, 154);
-  static const green = Color.fromARGB(255, 157, 255, 129);
-  static const sgreen = Color(0xFF83DE6C);
-  static const blue = Color.fromARGB(255, 129, 141, 255);
+  static const red = Color.fromARGB(255, 247, 168, 168);
+  static const green = Color.fromARGB(255, 184, 247, 167);
+  static const sgreen = Color.fromARGB(255, 154, 221, 137);
+  static const blue = Color.fromARGB(255, 169, 177, 249);
   static const sblue = Color.fromARGB(255, 132, 143, 226);
-  static const yellow = Color.fromARGB(255, 253, 255, 128);
-  static const syellow = Color.fromARGB(255, 222, 218, 130);
+  static const yellow = Color.fromARGB(255, 246, 248, 168);
+  static const syellow = Color.fromARGB(255, 244, 232, 0);
 }
