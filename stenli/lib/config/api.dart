747388834f2c2a157d3f11522bba33f925f79208
David Diamanta <dantadiamanta@gmail.com>
2023-01-03 08:52:02 +0700

    change api from local to web server

--- stenli/lib/config/api.dart
+++ stenli/lib/config/api.dart
@@ -1,5 +1,5 @@
 class Api {
-  static const baseUrl = 'http://192.168.1.11/stenli';
+  static const baseUrl = 'http://doja.my.id/stenli/api/';
 
   static const user = '$baseUrl/user';
   static const pemasukan = '$baseUrl/pemasukan';
