2441103c1f96fc66cf3367f701ccb59406f3a152
David Diamanta <dantadiamanta@gmail.com>
2023-01-08 00:00:29 +0700

    add empty Akun, Kritik, and Tentang Pages

---
+++ stenli/lib/presentation/page/profile/akun_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AkunPage extends StatefulWidget {
+  @override
+  State<AkunPage> createState() => _AkunPageState();
+}
+
+class _AkunPageState extends State<AkunPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Akun'),
+      ),
+    );
+  }
+}
