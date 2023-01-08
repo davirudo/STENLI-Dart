af462abfa4fe297f2e7e92d374c7bb3e1050835a
David Diamanta <dantadiamanta@gmail.com>
2023-01-08 20:15:53 +0700

    progressing KritikPage

--- stenli/lib/presentation/page/profile/kritik_page.dart
+++ stenli/lib/presentation/page/profile/kritik_page.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:stenli/config/app_fonts.dart';
+
+import '../../../config/session.dart';
+import '../profile_page.dart';
 
 class KritikPage extends StatefulWidget {
   KritikPage({Key? key}) : super(key: key);
@@ -10,6 +16,31 @@ class KritikPage extends StatefulWidget {
 class _KritikPageState extends State<KritikPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Masih dalam masa pengembangan.",
+              style: AppFonts.top,
+            ),
+            Text(
+              "ditunggu untuk updatenya ya~",
+              style: AppFonts.top.copyWith(fontSize: 18),
+            ),
+            SizedBox(
+              height: 100,
+            ),
+            TextButton(
+                onPressed: () {
+                  Session.clearUser;
+                  Get.off(() => ProfilePage());
+                },
+                child: Text("klik disini untuk kembali")),
+          ],
+        ),
+      ),
+    );
   }
 }
