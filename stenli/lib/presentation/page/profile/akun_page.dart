4324cb64a7ea31fb971d622376a01d6b6dda9ad9
David Diamanta <dantadiamanta@gmail.com>
2023-01-08 20:15:35 +0700

    progressing AkunPage

--- stenli/lib/presentation/page/profile/akun_page.dart
+++ stenli/lib/presentation/page/profile/akun_page.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:stenli/config/app_color.dart';
+import 'package:stenli/presentation/page/profile_page.dart';
+import '../../../config/app_fonts.dart';
+import '../../../config/session.dart';
 
 class AkunPage extends StatefulWidget {
   @override
@@ -10,8 +14,63 @@ class _AkunPageState extends State<AkunPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Akun'),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 10, right: 20.0, bottom: 10, top: 5),
+            child: Row(
+              children: [
+                Expanded(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          Session.clearUser;
+                          Get.off(() => ProfilePage());
+                        },
+                        icon: Icon(
+                          Icons.arrow_back_sharp,
+                          color: AppColor.primary,
+                        ),
+                      ),
+                      Text(
+                        "Akun Saya",
+                        style: AppFonts.top,
+                      ),
+                      Icon(Icons.more_vert, color: Colors.transparent),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 20),
+          // Column(
+          //   children: [
+          //     Text(
+          //       "Data Akun",
+          //       style: AppFonts.top,
+          //     ),
+          //     Container(
+          //       decoration: BoxDecoration(
+          //         color: AppColor.secondary,
+          //       ),
+          //       child: Column(
+          //         children: [
+          //           Text(
+          //             "Nama Lengkap",
+          //             style: AppFonts.title.copyWith(
+          //               color: AppColor.primary,
+          //             ),
+          //           ),
+          //         ],
+          //       ),
+          //     ),
+          //   ],
+          // ),
+        ],
       ),
     );
   }
