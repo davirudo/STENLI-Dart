4fab4c4e866bdfea28c315156144961849443664
David Diamanta <dantadiamanta@gmail.com>
2023-01-13 14:13:27 +0700

    add some textstyle

--- stenli/lib/config/app_theme.dart
+++ stenli/lib/config/app_theme.dart
@@ -1,72 +1,131 @@
-import 'package:flutter/material.dart'; 
-import 'package:google_fonts/google_fonts.dart'; 
-  
-  //Ini belum dipake, cuman referensi aja
-  //nanti color dan fontnya mau digabungn kesini
-  //karena kalo dipisah gtu bisa ngganggu performa aplikasi
- Color kPrimaryColor = Color(0xff1E387F);
- Color kSecondaryColor = Color.fromARGB(255, 137, 160, 222);
- Color kBlackColor = Color(0xff222222); 
- Color kBlackAccentColor = Color(0xff2A2B37); 
- Color kGreyColor = Color(0xff94959B); 
- Color kLineDarkColor = Color(0xffEAEAEA); 
- Color kWhiteGreyColor = Color(0xffF6F6F6); 
- Color kWhiteColor = Color(0xffFFFFFF); 
- Color kRedColor = Color(0xffFF314A); 
- Color kYellowColor = Color(0xffFFBA00); 
- Color kBlueColor = Color(0xff2972FF); 
- Color kDarkBlueColor = Color(0xff16558F); 
- Color kBlueaccentColor = Color(0xffDEF4FF); 
- Color kDarkBackgroundColor = Color(0xff252836); 
- Color kgreenaccentcolor = Color(0xffEBFFF3); 
- Color kdarkcolor = Color(0xff0C3851); 
- Color kchococolor = Color(0xffA96405); 
- Color kgreencolor = Color(0xff477709); 
- Color kskybluecolor = Color(0xff068878); 
- Color kpurplecolor = Color(0xff5C1588); 
- Color kredaccentcolo = Color(0xffCB504B);
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
- TextStyle primaryTextStyle = GoogleFonts.poppins(
+//Ini belum dipake, cuman referensi aja
+//nanti color dan fontnya mau digabungn kesini
+//karena kalo dipisah gtu bisa ngganggu performa aplikasi
+Color kPrimaryColor = Color(0xff1E387F);
+Color kSecondaryColor = Color.fromARGB(255, 137, 160, 222);
+Color kBlackColor = Color(0xff222222);
+Color kBlackAccentColor = Color(0xff2A2B37);
+Color kGreyColor = Color(0xff94959B);
+Color kLineDarkColor = Color(0xffEAEAEA);
+Color kWhiteGreyColor = Color(0xffF6F6F6);
+Color kWhiteColor = Color(0xffFFFFFF);
+Color kRedColor = Color(0xffFF314A);
+Color kYellowColor = Color(0xffFFBA00);
+Color kBlueColor = Color(0xff2972FF);
+Color kDarkBlueColor = Color(0xff16558F);
+Color kBlueaccentColor = Color(0xffDEF4FF);
+Color kDarkBackgroundColor = Color(0xff252836);
+Color kgreenaccentcolor = Color(0xffEBFFF3);
+Color kdarkcolor = Color(0xff0C3851);
+Color kchococolor = Color(0xffA96405);
+Color kgreencolor = Color(0xff477709);
+Color kskybluecolor = Color(0xff068878);
+Color kpurplecolor = Color(0xff5C1588);
+Color kredaccentcolo = Color(0xffCB504B);
+
+TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: kPrimaryColor,
-); 
+);
 
- TextStyle secondaryTextStyle = GoogleFonts.poppins(
+TextStyle secondaryTextStyle = GoogleFonts.poppins(
   color: kSecondaryColor,
-); 
- 
- TextStyle blackTextStyle = GoogleFonts.poppins( 
- color: kBlackColor, 
- ); 
- TextStyle blackAccentTextStyle = GoogleFonts.poppins( 
- color: kBlackAccentColor, 
- ); 
- TextStyle greyTextStyle = GoogleFonts.poppins( 
- color: kGreyColor, 
- ); 
- TextStyle lineDarkTextStyle = GoogleFonts.poppins( 
- color: kLineDarkColor, 
- ); 
- TextStyle whiteGreyTextStyle = GoogleFonts.poppins( 
- color: kWhiteGreyColor, 
- ); 
- TextStyle whiteTextStyle = GoogleFonts.poppins( 
- color: kWhiteColor, 
- ); 
- TextStyle redTextStyle = GoogleFonts.poppins( 
- color: kRedColor, 
- ); 
- TextStyle yellowTextStyle = GoogleFonts.poppins( 
- color: kYellowColor, 
- ); 
- TextStyle blueTextStyle = GoogleFonts.poppins( 
- color: kBlueColor, 
- ); 
- TextStyle darkblueTextStyle = GoogleFonts.poppins( 
-color: kDarkBlueColor, 
- ); 
-  
- FontWeight light = FontWeight.w300; 
- FontWeight regular = FontWeight.w400; 
- FontWeight medium = FontWeight.w500; 
- FontWeight semiBold = FontWeight.w800; 
- FontWeight bold = FontWeight.w900;
+);
+
+TextStyle blackTextStyle = GoogleFonts.poppins(
+  color: kBlackColor,
+);
+TextStyle blackAccentTextStyle = GoogleFonts.poppins(
+  color: kBlackAccentColor,
+);
+TextStyle greyTextStyle = GoogleFonts.poppins(
+  color: kGreyColor,
+);
+TextStyle lineDarkTextStyle = GoogleFonts.poppins(
+  color: kLineDarkColor,
+);
+TextStyle whiteGreyTextStyle = GoogleFonts.poppins(
+  color: kWhiteGreyColor,
+);
+TextStyle whiteTextStyle = GoogleFonts.poppins(
+  color: kWhiteColor,
+);
+TextStyle redTextStyle = GoogleFonts.poppins(
+  color: kRedColor,
+);
+TextStyle yellowTextStyle = GoogleFonts.poppins(
+  color: kYellowColor,
+);
+TextStyle blueTextStyle = GoogleFonts.poppins(
+  color: kBlueColor,
+);
+TextStyle darkblueTextStyle = GoogleFonts.poppins(
+  color: kDarkBlueColor,
+);
+
+TextStyle top = GoogleFonts.poppins(
+  color: kPrimaryColor,
+  fontWeight: FontWeight.w700,
+  fontSize: 20,
+);
+
+TextStyle title = GoogleFonts.poppins(
+  color: kWhiteColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 16,
+);
+
+TextStyle desc = GoogleFonts.poppins(
+  color: kWhiteColor,
+  fontWeight: FontWeight.w500,
+  fontSize: 18,
+);
+
+TextStyle peepee = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: kWhiteColor,
+);
+
+TextStyle peepeegrey = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: Colors.grey,
+);
+
+TextStyle peepeeblack = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w600,
+  color: Colors.black,
+);
+
+TextStyle poopoo = GoogleFonts.poppins(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: Colors.white70,
+);
+
+TextStyle details = GoogleFonts.poppins(
+  fontSize: 12,
+  color: kBlackAccentColor,
+);
+
+TextStyle card2 = GoogleFonts.poppins(
+  fontSize: 20,
+  fontWeight: FontWeight.w500,
+  color: kWhiteColor,
+);
+
+TextStyle card3 = GoogleFonts.poppins(
+  fontSize: 18,
+  fontWeight: FontWeight.w400,
+  color: kWhiteColor,
+);
+
+FontWeight light = FontWeight.w300;
+FontWeight regular = FontWeight.w400;
+FontWeight medium = FontWeight.w500;
+FontWeight semiBold = FontWeight.w800;
+FontWeight bold = FontWeight.w900;
